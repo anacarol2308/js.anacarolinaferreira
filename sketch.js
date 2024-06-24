f3e63ba810c9c9a3321eae4eced1e0f59852faf5
anacarol2308 <00001117220321sp@al.educacao.sp.gov.br>
2024-06-24 08:18:27 -0300

    Adicionando arquivos js e css

---
+++ sketch.js
@@ -0,0 +1,14 @@
+function setup() {
+    createCanvas(400, 400);
+    background("black");
+  }
+  
+  function draw() {
+   stroke ("blue");
+   fill ("red");
+
+  if (mouseIspressed){
+    rect(mouseX,mouseY,20,35);
+  }
+
+  }
